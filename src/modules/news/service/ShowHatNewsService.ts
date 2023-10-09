2080485fb887b49c741bebd05c03bfce406fd702
matheusjean <matheusjean11@hotmail.com>
2023-10-09 20:11:01 -0300

    feat: change getByHat to return categories

--- src/modules/news/service/ShowHatNewsService.ts
+++ src/modules/news/service/ShowHatNewsService.ts
@@ -10,6 +10,20 @@ class ShowHatNewsService {
   public static async execute({ hat }: IRequest): Promise<News | undefined> {
     const newsRepository = getCustomRepository(NewsRepository);
 
+    const news = await newsRepository
+      .createQueryBuilder('news')
+      .leftJoinAndSelect('news.categories', 'categories')
+      .where('news.hat = :hat', { hat })
+      .getOne();
+
+    return news;
+  }
+
+  public static async findNewsByHat({
+    hat,
+  }: IRequest): Promise<News | undefined> {
+    const newsRepository = getCustomRepository(NewsRepository);
+
     const news = await newsRepository.findByName(hat);
 
     return news;
